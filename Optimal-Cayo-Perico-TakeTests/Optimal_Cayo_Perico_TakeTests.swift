cfc1215df9a55cee767e22d03e946d8522b9733e
Danny Paez <danny_paez@intuit.com>
2021-03-19 11:42:30 -0700

    Initial Commit

---
+++ Optimal-Cayo-Perico-TakeTests/Optimal_Cayo_Perico_TakeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Optimal_Cayo_Perico_TakeTests.swift
+//  Optimal-Cayo-Perico-TakeTests
+//
+//  Created by Danny Paez on 3/19/21.
+//  Copyright © 2021 Danny Paez. All rights reserved.
+//
+
+import XCTest
+@testable import Optimal_Cayo_Perico_Take
+
+class Optimal_Cayo_Perico_TakeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
